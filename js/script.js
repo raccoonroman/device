cd7cec56071c04e5ce2c8ea63a11f1952f78f732
Roman <34542700+raccoonroman@users.noreply.github.com>
2018-06-17 19:40:05 +0300

    added services

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 var slideIndex = 1;
 showSlides(slideIndex);
+showServicesSlides(slideIndex);
 
 function slide(n) {
 	showSlides(slideIndex = n);
+}
+function serviceSlide(n) {
+	showServicesSlides(slideIndex = n);
 }
 
 function showSlides(n) {
@@ -17,4 +21,18 @@ function showSlides(n) {
 	}
 	slides[slideIndex-1].style.display = "flex";
 	dots[slideIndex-1].className += " active";
+}
+
+function showServicesSlides(n) {
+	var i;
+	var servicesSlides = document.getElementsByClassName("services-slider-item");
+	var servicesBtns = document.getElementsByClassName("services-btn");
+	for (i = 0; i < servicesSlides.length; i++) {
+		servicesSlides[i].style.display = "none"; 
+	}
+	for (i = 0; i < servicesBtns.length; i++) {
+		servicesBtns[i].className = servicesBtns[i].className.replace(" active", "");
+	}
+	servicesSlides[slideIndex-1].style.display = "block"; 
+	servicesBtns[slideIndex-1].className += " active";
 }
